4596d910f27b1a107f143848bb3b81f9f4c34016
Dmytro Beshchuk <dmytro.beshchuk@protonmail.com>
2026-02-18 10:42:59 +0200

    initial commit

---
+++ src/views/chats/Chats_List.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+		<UserList />
+	</div>
+</template>
+
+<style lang="scss" scoped></style>
+
+<script setup>
+import UserList from '../../components/UserList.vue';
+</script>
---
+++ src/views/menu/views/Menu_Chats.vue
@@ -0,0 +1,17 @@
+<template>
+	<Chats_List />
+</template>
+
+<script setup>
+import Chats_List from '@/views/chats/Chats_List.vue';
+import { ref, inject } from 'vue';
+
+const $router = inject('$router');
+const $user = inject('$user');
+const selected = ref([]);
+
+const select = (address) => {
+	selected.value = [address];
+	$router.push({ name: 'chat', params: { address } });
+};
+</script>
